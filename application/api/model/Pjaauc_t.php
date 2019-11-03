46c0df1b656cd5c5550ea989217f17ab102fa9e7
super <kdwmcyzw@163.com>
2019-11-03 21:27:47 +0800

    timeFormat

--- application/api/model/Pjaauc_t.php
+++ application/api/model/Pjaauc_t.php
@@ -18,9 +18,9 @@ class Pjaauc_t extends BaseModel
         $map['PJAAUC136'] = 'Y';
 
         $projects = self::where($map)
-                ->field('PJAAUC073 as projectNo,PJAAUC010 as projectShotName
+                ->field("PJAAUC073 as projectNo,PJAAUC010 as projectShotName
                 ,PJAAUC106 as period,PJAAUC132 as status,PJAAUC017 as pm
-                ,PJAAUC013 as area,PJAAUC066 as onlineDate')
+                ,PJAAUC013 as area,to_char(PJAAUC066,'yyyy-mm-dd') as onlineDate")
                 ->select();
 //            项目编号、项目简称、13项目地区、17 PM、106项目阶段、132项目状态、066预计上线日期
 //                ->visible(['PJAAUC073','PJAAUC010','PJAAUC017','PJAAUC066']);
